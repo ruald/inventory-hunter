81914eeeba699d2c3791a12842fffe19a37ad271
ruald <ruald@live.com>
2021-04-20 20:02:05 +1000

    Update config.py

--- src/config.py
+++ src/config.py
@@ -33,6 +33,8 @@ class Config:
                 netloc = netloc.replace('www.', '')
             if netloc.endswith('.com'):
                 netloc = netloc.replace('.com', '')
+            if netloc.endswith('.com.au'):
+                netloc = netloc.replace('.com.au', '')
             for c in ('a', 'e', 'i', 'o', 'u'):
                 netloc = netloc.replace(c, '')
             netloc = netloc.replace('.', '_')
